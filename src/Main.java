b592eea896407b77e5df913d942d8bc4cdace7fe
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2019-04-12 18:26:49 +0300

    Added gitingnore file and Receiver, Transceiver classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Transceiver transceiver = new Transceiver("11011");
     }
 }
---
+++ src/Transceiver.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Transceiver {
+
+    private String key;
+    private String data;
+    private String dataWithKey;
+    private String dataWithCrc;
+    private int lenOfKey;
+
+    public Transceiver(String key) {
+        this.key = key;
+        lenOfKey = (int) key.length();
+        generateData();
+        generateDataWithKey();
+        division();
+    }
+
+    private void generateData() {
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        //long randomData = ThreadLocalRandom.current().nextLong(Long.parseLong(key, 2), 100000 + 1);
+        //data = Long.toBinaryString(randomData);
+        data = "11100101";
+        //System.out.println(data + "\n" + key);
+    }
+
+    private void generateDataWithKey() {
+        dataWithKey = data;
+        for (long i=0; i<lenOfKey-1; i++) {
+            dataWithKey = dataWithKey + "0";
+        }
+        //System.out.println(dataWithKey);
+    }
+
+    private void division() {
+        String value;
+        String result;
+        while (dataWithKey.length()>0) {
+            value = dataWithKey.substring(0,lenOfKey);
+
+        }
+    }
+}
