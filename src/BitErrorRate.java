e0566678e159ff9180ba26623ebc875b0a509ae5
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2019-04-12 21:19:55 +0300

    BitErrorRate class complete

---
+++ src/BitErrorRate.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class BitErrorRate {
+
+    private String data;
+    private boolean hasBitErrorRate;
+
+    public BitErrorRate(String data) {
+        this.data = data;
+        Random random = new Random();
+        hasBitErrorRate = random.nextBoolean();
+    }
+
+    public String start() {
+        if (hasBitErrorRate) {
+            generateBitErrorRate();
+        }
+        return data;
+    }
+
+    private void generateBitErrorRate() {
+        Random random = new Random();
+        Random errorBit = new Random();
+        int numberOfErrorBit = random.nextInt(data.length()+1);
+        int indexOfError;
+        for (int i=0; i<numberOfErrorBit; i++) {
+            indexOfError = random.nextInt(data.length());
+            data = data.substring(0, indexOfError) + String.valueOf(errorBit.nextInt(2)) +
+                    data.substring(indexOfError+1);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         Transceiver transceiver = new Transceiver("11011", "11100101");
-        Receiver receiver = new Receiver("11011", transceiver.start());
-        System.out.println(transceiver.start());
+        BitErrorRate bitErrorRate = new BitErrorRate(transceiver.start());
+        Receiver receiver = new Receiver("11011", bitErrorRate.start());
         System.out.println(receiver.start());
     }
 }
