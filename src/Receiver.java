6738c00a7e7a074de90fb2c5638e0ff0c28a996b
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2019-04-12 20:16:14 +0300

    Receiver class -> complete

--- src/Receiver.java
+++ src/Receiver.java
@@ -2,11 +2,29 @@ public class Receiver {
 
     private String key;
     private String data;
+    private int lenOfKey;
 
     public Receiver(String key, String data) {
         this.key = key;
         this.data = data;
+        lenOfKey = key.length();
     }
 
-    private void division() {}
+    public String start() {
+        division();
+        if (data.contains("1")) {
+            return "Wrong data!";
+        }
+        return "Correct data!";
+    }
+
+    private void division() {
+        String value;
+        String result;
+        while (data.length()>=lenOfKey) {
+            value = data.substring(0, lenOfKey);
+            result = Long.toBinaryString(Long.parseLong(value, 2) ^ Long.parseLong(key, 2));
+            data = result + data.substring(lenOfKey);
+        }
+    }
 }
